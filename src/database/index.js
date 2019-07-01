c3ec997910f907aa4ccc23372211b8946db3d3de
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-01 15:53:59 -0300

    criado o arquivo de conexão com banco de dados

---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // conexão com o banco de dados
+    this.connection = new Sequelize(databaseConfig);
+
+    models.forEach(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
