f5c176ccd2184e2ea12602b658edf02479327f19
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-01 15:11:52 -0300

    adcionado o prettier => yarn add prettier eslint-config-prettier eslint-plugin-prettier -D

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ status: "online" });
+routes.get('/', (req, res) => {
+  return res.json({ status: 'online' });
 });
 
 export default routes;
