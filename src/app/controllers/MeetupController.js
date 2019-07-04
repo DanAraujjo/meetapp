38545045916990f7cdef093697cd54a78b10e6c5
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-04 16:57:04 -0300

    listar os meetups que são organizados pelo usuário logado

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,10 +1,13 @@
 import * as Yup from 'yup';
 import { parseISO, isBefore } from 'date-fns';
-import { Op } from 'sequelize';
+// import { Op } from 'sequelize';
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class MeetupController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const meetup = await Meetup.findAll({
       where: {
         user_id: req.userId,
@@ -13,6 +16,17 @@ class MeetupController {
           [Op.gte]: new Date(),
         }, */
       },
+      order: ['date'],
+      attributes: ['id', 'title', 'description', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: File,
+          as: 'banner',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
     });
 
     return res.json(meetup);
