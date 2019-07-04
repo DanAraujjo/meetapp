68c3e61de820e15183fb3e219198e0e69493874a
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-04 16:47:26 -0300

    alteração de eventos

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,10 +1,21 @@
 import * as Yup from 'yup';
 import { parseISO, isBefore } from 'date-fns';
+import { Op } from 'sequelize';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async index(req, res) {
-    return res.json();
+    const meetup = await Meetup.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+        /* date: {
+          [Op.gte]: new Date(),
+        }, */
+      },
+    });
+
+    return res.json(meetup);
   }
 
   async store(req, res) {
@@ -40,6 +51,60 @@ class MeetupController {
       date,
       banner_id,
     });
+
+    return res.status(201).json(meetup);
+  }
+
+  async update(req, res) {
+    if (!req.params.id) {
+      return res.status(400).json({ error: 'Id de evento inválido!' });
+    }
+
+    // validar os dados informados na requisição
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      banner_id: Yup.number().required(),
+    });
+
+    // verificar se o corpo da requisição é valida
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ message: 'Validação falhou!' });
+    }
+
+    // localizar evento
+    const meetup = await Meetup.findOne({
+      where: {
+        id: req.params.id,
+        user_id: req.userId,
+        canceled_at: null,
+        /* date: {
+          [Op.gte]: new Date(),
+        }, */
+      },
+    });
+
+    if (!meetup) {
+      return res.status(401).json({ message: 'Evento não localizado!' });
+    }
+
+    // verifica se ja passou a data e se é possivel alteração do evento
+    if (isBefore(meetup.date, new Date())) {
+      return res.status(400).json({
+        error: 'Ops! Esse evento já ocorreu, não é permitido alteração.',
+      });
+    }
+
+    // verificar se a nova data ja passou
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({
+        error: 'Não é permitido agendamento para data/hora passada.',
+      });
+    }
+
+    await meetup.update(req.body);
 
     return res.json(meetup);
   }
